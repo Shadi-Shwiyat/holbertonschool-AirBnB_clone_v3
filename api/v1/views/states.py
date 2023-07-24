77f94dc151fbd7f38f8e4919e92766ffa6ec839d
Shadi-Shwiyat <raz611103@gmail.com>
2023-07-24 14:33:44 -0700

    Added function that returns specific state object based on state_id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,7 +5,7 @@
 from models import State
 from models import storage
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, abort
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
@@ -16,3 +16,13 @@ def return_states():
     for key, value in states.items():
         states_list.append(value.to_dict())
     return jsonify(states_list)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def return_states_id(state_id):
+    """ Return a specific state based on given id using GET """
+    states = storage.all(State)
+    for key, value in states.items():
+        if states[key].id == state_id:
+            return value.to_dict()
+    abort(404)
