25c2c2f34bacafc7dabf0ec7bf3cc3ab3d288afb
Shadi-Shwiyat <raz611103@gmail.com>
2023-07-23 22:52:51 -0700

    Added function to return # of each object by type to index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,13 @@
 
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -10,3 +17,15 @@ def status():
     """ Returns the status of GET request """
     status = {"status": "OK"}
     return jsonify(status)
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ Returns the number of each object by type"""
+    return jsonify({
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User)
+        })
