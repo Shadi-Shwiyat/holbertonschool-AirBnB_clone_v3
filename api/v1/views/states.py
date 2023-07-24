083416043507a5b0e54cb2a2e3a9516156e0fe3a
Shadi-Shwiyat <raz611103@gmail.com>
2023-07-24 15:18:14 -0700

    Added function to update state object using PUT

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -56,3 +56,24 @@ def create_states():
         storage.new(obj)
         storage.save()
         return (jsonify(obj.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_states(state_id):
+    """ Update a state objest using PUT """
+    body = request.get_json()
+    if body is None:
+        return (jsonify({'error': 'Not a JSON'}), 400)
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    else:
+        ignore_key = ['id', 'created_at', 'updated_at']
+        for key, value in body.items():
+            if key not in ignore_key:
+                setattr(state, key, value)
+            else:
+                pass
+        storage.save()
+        return (jsonify(state.to_dict()), 200)
